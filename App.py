72aeb38348745621c3a43f265cb3caeca38e893c
Varshaaa17 <163492914+Varshaaa17@users.noreply.github.com>
2025-09-22 12:55:40 +0530

    Update and rename app.py to App.py

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template, request
-app=Flask(_name_)
+app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template("form.html")
@@ -10,5 +10,5 @@ def submit():
     year=request.form.get('year')
     phone=request.form.get('phone')
     return render_template('result.html',name=username,email=email,year=year,phone=phone)
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host='0.0.0.0',port="5001",debug=True)
